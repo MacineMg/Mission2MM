14110b1cea752bca2f0550d118096c5d5cf8df4a
MacineMg <123747921+MacineMg@users.noreply.github.com>
2024-04-24 21:41:27 +0200

    Initial commit

---
+++ resources/views/modeles/nvxVisiteur.php
@@ -0,0 +1,34 @@
+<h1>Nouveaux visiteur?</h1>
+<form method="post" action="form.php">
+  <div class="champ">
+    <label for="prenom">Prénom : </label>
+    <input type="text" id="prenom" name="prenom">
+  </div>
+  <div class="champ">
+    <label for="nom">Nom : </label>
+    <input type="text" id="nom" name="nom">
+  </div>
+  <div class="champ">
+    <label for="pass">Entrez un mot de passe :</label>
+    <input type="password" id="pass" name="pass">
+  </div>
+  <div class="champ">
+    <label for="mail">Entrez un mail :</label>
+    <input type="email" id="mail" name="mail">
+  </div>
+  <div class="champ">
+    <label for="adresse">adresse : </label>
+    <input type="text" id="adresse" name="adresse">
+  </div>
+  <div class="champ">
+    <label for="code postale">code postale : </label>
+    <input type="code postale" id="code postale" name="code postale">
+  </div>
+  <div class="champ">
+    <label for="date embauche">date embauche : </label>
+    <input type="text" id="date embauche" name="date embauche">
+  </div>
+  <div class="champ">
+    <input type="submit" value="Envoyer">
+   </div>
+</form>
